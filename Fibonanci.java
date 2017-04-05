4da029426a262f67d5994c88d818c133c81d22bd
Monica Liviandra <monicaliviandra@gmail.com>
2017-04-05 09:22:21 +0700

    merubah tulisan
    merubah masukkan deret fibonnanci

--- Fibonanci.java
+++ Fibonanci.java
@@ -19,7 +19,7 @@ public class Fibonanci {
     public static void main(String[] args) {
           int a=0,b=1,hasil=0,c;
    Scanner input = new Scanner (System.in);
-   System.out.print ("Masukan Deret Fibonacci  : ");
+   System.out.print ("Masukan Deret Terbalik Fibonacci  : ");
         c = input.nextInt ();
         for(int i=1; i<=c; i++){            
     a=b;
